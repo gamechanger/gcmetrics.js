835621c6e4fc26c5a2d6b37c7e6b44e8383734df
Miroslav Shubernetskiy <miroslav@miki725.com>
2020-12-30 15:26:25 -0500

    adding gitlab ci

--- test/metrics_test.js
+++ test/metrics_test.js
@@ -1,202 +1,229 @@
-var _ = require('underscore'),
-    StatsD = require('../lib/statsd'),
-    config = require('@gamechanger/config');
-    sinon = require('sinon');
+const metrics = require('../lib/metrics');
+const sinon = require('sinon');
 
-config.dogstatsd = {
-    port: 1234,
-    localhost: 'localhost'
-};
+const realStatsD = metrics.registerDogStatsD(
+    'localhost',
+    1234,
+    'customPrefix.',
+);
 
-var real = StatsD.create();
-var mockStatsD;
+describe('Metric logging', () => {
+  let mockStatsD;
 
+  beforeEach(() => {
+    mockStatsD = sinon.mock(realStatsD);
+  });
 
-StatsD.create = function() {
-    return real;
-};
-
-var metrics = require('../lib/metrics');
-
-describe('Metric logging', function() {
-
-    after(function() {
-        delete require.cache[require.resolve('../lib/statsd')];
+  describe('timing', () => {
+    it('should send timing info with sample rate and tags as array', () => {
+      mockStatsD.expects('timing').once()
+          .withArgs('customPrefix.something', 50, 0.5, ['test']);
+      metrics.timing('something', 50, 0.5, ['test']);
+      mockStatsD.verify();
     });
 
-    beforeEach(function() {
-        mockStatsD = sinon.mock(real);
+    it('should send timing info with tags as array', () => {
+      mockStatsD.expects('timing').once()
+          .withArgs('customPrefix.something', 50, 1, ['test']);
+      metrics.timing('something', 50, ['test']);
+      mockStatsD.verify();
     });
 
-
-    describe('timing', function() {
-        it("should send timing info with sample rate and tags as array", function() {
-            mockStatsD.expects('timing').once().withArgs('@gamechanger/metrics.something', 50, 0.5, ['test']);
-            metrics.timing('something', 50, 0.5, ['test']);
-            mockStatsD.verify();
-        });
-
-        it("should send timing info with tags as array", function() {
-            mockStatsD.expects('timing').once().withArgs('@gamechanger/metrics.something', 50, 1, ['test']);
-            metrics.timing('something', 50, ['test']);
-            mockStatsD.verify();
-        });
-
-        it("should send timing info with sample rate and tags as object", function() {
-            mockStatsD.expects('timing').once().withArgs('@gamechanger/metrics.something', 50, 0.5, ['foo:bar']);
-            metrics.timing('something', 50, 0.5, {foo: 'bar'});
-            mockStatsD.verify();
-        });
-
-        it("should send timing info with tags as object", function() {
-            mockStatsD.expects('timing').once().withArgs('@gamechanger/metrics.something', 50, 1, ['foo:bar']);
-            metrics.timing('something', 50, {foo: 'bar'});
-            mockStatsD.verify();
-        });
+    it('should send timing info with sample rate and tags as object', () => {
+      mockStatsD.expects('timing').once()
+          .withArgs('customPrefix.something', 50, 0.5, ['foo:bar']);
+      metrics.timing('something', 50, 0.5, {foo: 'bar'});
+      mockStatsD.verify();
     });
 
-    describe('increment', function() {
-        it("should increment with a sample rate and tag array", function() {
-            mockStatsD.expects('increment').withArgs('@gamechanger/metrics.something', 0.5, ['test']);
-            metrics.increment('something', 0.5, ['test']);
-            mockStatsD.verify();
-        });
-        it("should increment with tag array", function() {
-            mockStatsD.expects('increment').withArgs('@gamechanger/metrics.something', 1, ['test']);
-            metrics.increment('something', ['test']);
-            mockStatsD.verify();
-        });
-        it("should increment with a sample rate and tag object", function() {
-            mockStatsD.expects('increment').withArgs('@gamechanger/metrics.something', 0.5, ['foo:bar']);
-            metrics.increment('something', 0.5, {foo: 'bar'});
-            mockStatsD.verify();
-        });
-        it("should increment with a sample rate and tag array", function() {
-            mockStatsD.expects('increment').withArgs('@gamechanger/metrics.something', 1, ['foo:bar']);
-            metrics.increment('something', {foo: 'bar'});
-            mockStatsD.verify();
-        });
+    it('should send timing info with tags as object', () => {
+      mockStatsD.expects('timing').once()
+          .withArgs('customPrefix.something', 50, 1, ['foo:bar']);
+      metrics.timing('something', 50, {foo: 'bar'});
+      mockStatsD.verify();
+    });
+  });
+
+  describe('increment', () => {
+    it('should increment with a sample rate and tag array', () => {
+      mockStatsD.expects('increment')
+          .withArgs('customPrefix.something', 0.5, ['test']);
+      metrics.increment('something', 0.5, ['test']);
+      mockStatsD.verify();
+    });
+    it('should increment with tag array', () => {
+      mockStatsD.expects('increment')
+          .withArgs('customPrefix.something', 1, ['test']);
+      metrics.increment('something', ['test']);
+      mockStatsD.verify();
+    });
+    it('should increment with a sample rate and tag object', () => {
+      mockStatsD.expects('increment')
+          .withArgs('customPrefix.something', 0.5, ['foo:bar']);
+      metrics.increment('something', 0.5, {foo: 'bar'});
+      mockStatsD.verify();
+    });
+    it('should increment with a sample rate and tag array', () => {
+      mockStatsD.expects('increment')
+          .withArgs('customPrefix.something', 1, ['foo:bar']);
+      metrics.increment('something', {foo: 'bar'});
+      mockStatsD.verify();
+    });
+  });
+
+  describe('decrement', () => {
+    it('should decrement with a sample rate and tag array', () => {
+      mockStatsD.expects('decrement')
+          .withArgs('customPrefix.something', 0.5, ['test']);
+      metrics.decrement('something', 0.5, ['test']);
+      mockStatsD.verify();
+    });
+    it('should decrement with tag array', () => {
+      mockStatsD.expects('decrement')
+          .withArgs('customPrefix.something', 1, ['test']);
+      metrics.decrement('something', ['test']);
+      mockStatsD.verify();
+    });
+    it('should decrement with a sample rate and tag object', () => {
+      mockStatsD.expects('decrement')
+          .withArgs('customPrefix.something', 0.5, ['foo:bar']);
+      metrics.decrement('something', 0.5, {foo: 'bar'});
+      mockStatsD.verify();
+    });
+    it('should decrement with a sample rate and tag array', () => {
+      mockStatsD.expects('decrement')
+          .withArgs('customPrefix.something', 1, ['foo:bar']);
+      metrics.decrement('something', {foo: 'bar'});
+      mockStatsD.verify();
+    });
+  });
+
+  describe('gauge', () => {
+    it('should send gauge info with sample rate and tags as array', () => {
+      mockStatsD.expects('gauge').once()
+          .withArgs('customPrefix.something', 50, 0.5, ['test']);
+      metrics.gauge('something', 50, 0.5, ['test']);
+      mockStatsD.verify();
     });
 
-    describe('decrement', function() {
-        it("should decrement with a sample rate and tag array", function() {
-            mockStatsD.expects('decrement').withArgs('@gamechanger/metrics.something', 0.5, ['test']);
-            metrics.decrement('something', 0.5, ['test']);
-            mockStatsD.verify();
-        });
-        it("should decrement with tag array", function() {
-            mockStatsD.expects('decrement').withArgs('@gamechanger/metrics.something', 1, ['test']);
-            metrics.decrement('something', ['test']);
-            mockStatsD.verify();
-        });
-        it("should decrement with a sample rate and tag object", function() {
-            mockStatsD.expects('decrement').withArgs('@gamechanger/metrics.something', 0.5, ['foo:bar']);
-            metrics.decrement('something', 0.5, {foo: 'bar'});
-            mockStatsD.verify();
-        });
-        it("should decrement with a sample rate and tag array", function() {
-            mockStatsD.expects('decrement').withArgs('@gamechanger/metrics.something', 1, ['foo:bar']);
-            metrics.decrement('something', {foo: 'bar'});
-            mockStatsD.verify();
-        });
+    it('should send gauge info with tags as array', () => {
+      mockStatsD.expects('gauge').once()
+          .withArgs('customPrefix.something', 50, 1, ['test']);
+      metrics.gauge('something', 50, ['test']);
+      mockStatsD.verify();
     });
 
-    describe('gauge', function() {
-        it("should send gauge info with sample rate and tags as array", function() {
-            mockStatsD.expects('gauge').once().withArgs('@gamechanger/metrics.something', 50, 0.5, ['test']);
-            metrics.gauge('something', 50, 0.5, ['test']);
-            mockStatsD.verify();
-        });
-
-        it("should send gauge info with tags as array", function() {
-            mockStatsD.expects('gauge').once().withArgs('@gamechanger/metrics.something', 50, 1, ['test']);
-            metrics.gauge('something', 50, ['test']);
-            mockStatsD.verify();
-        });
-
-        it("should send gauge info with sample rate and tags as object", function() {
-            mockStatsD.expects('gauge').once().withArgs('@gamechanger/metrics.something', 50, 0.5, ['foo:bar']);
-            metrics.gauge('something', 50, 0.5, {foo: 'bar'});
-            mockStatsD.verify();
-        });
-
-        it("should send gauge info with tags as object", function() {
-            mockStatsD.expects('gauge').once().withArgs('@gamechanger/metrics.something', 50, 1, ['foo:bar']);
-            metrics.gauge('something', 50, {foo: 'bar'});
-            mockStatsD.verify();
-        });
+    it('should send gauge info with sample rate and tags as object', () => {
+      mockStatsD.expects('gauge').once()
+          .withArgs('customPrefix.something', 50, 0.5, ['foo:bar']);
+      metrics.gauge('something', 50, 0.5, {foo: 'bar'});
+      mockStatsD.verify();
     });
 
-    describe('histogram', function() {
-        it("should send histogram info with sample rate and tags as array", function() {
-            mockStatsD.expects('histogram').once().withArgs('@gamechanger/metrics.something', 50, 0.5, ['test']);
-            metrics.histogram('something', 50, 0.5, ['test']);
-            mockStatsD.verify();
-        });
+    it('should send gauge info with tags as object', () => {
+      mockStatsD.expects('gauge').once()
+          .withArgs('customPrefix.something', 50, 1, ['foo:bar']);
+      metrics.gauge('something', 50, {foo: 'bar'});
+      mockStatsD.verify();
+    });
+  });
 
-        it("should send histogram info with tags as array", function() {
-            mockStatsD.expects('histogram').once().withArgs('@gamechanger/metrics.something', 50, 1, ['test']);
-            metrics.histogram('something', 50, ['test']);
-            mockStatsD.verify();
-        });
-
-        it("should send histogram info with sample rate and tags as object", function() {
-            mockStatsD.expects('histogram').once().withArgs('@gamechanger/metrics.something', 50, 0.5, ['foo:bar']);
-            metrics.histogram('something', 50, 0.5, {foo: 'bar'});
-            mockStatsD.verify();
-        });
-
-        it("should send histogram info with tags as object", function() {
-            mockStatsD.expects('histogram').once().withArgs('@gamechanger/metrics.something', 50, 1, ['foo:bar']);
-            metrics.histogram('something', 50, {foo: 'bar'});
-            mockStatsD.verify();
-        });
+  describe('histogram', () => {
+    it('should send histogram info with sample rate and tags as array', () => {
+      mockStatsD.expects('histogram').once()
+          .withArgs('customPrefix.something', 50, 0.5, ['test']);
+      metrics.histogram('something', 50, 0.5, ['test']);
+      mockStatsD.verify();
     });
 
-    describe('timed callback', function() {
-        it('should time callbacks with sample rate and tag array', function(done) {
-            mockStatsD.expects('timing').once().withArgs('@gamechanger/metrics.something', sinon.match.number, 0.5, ['test']);
-            metrics.timed(function(cb) {
-                setTimeout(function() {
-                    cb('something', 0.5, ['test']);
-                    mockStatsD.verify();
-                    done();
-                }, 10);
-            });
-        });
-
-        it('should time callbacks with tag array', function(done) {
-            mockStatsD.expects('timing').once().withArgs('@gamechanger/metrics.something', sinon.match.number, 1, ['test']);
-            metrics.timed(function(cb) {
-                setTimeout(function() {
-                    cb('something', ['test']);
-                    mockStatsD.verify();
-                    done();
-                }, 10);
-            });
-        });
-
-        it('should time callbacks with sample rate and tag object', function(done) {
-            mockStatsD.expects('timing').once().withArgs('@gamechanger/metrics.something', sinon.match.number, 0.5, ['foo:bar']);
-            metrics.timed(function(cb) {
-                setTimeout(function() {
-                    cb('something', 0.5, {foo:'bar'});
-                    mockStatsD.verify();
-                    done();
-                }, 10);
-            });
-        });
-
-        it('should time callbacks with tag object', function(done) {
-            mockStatsD.expects('timing').once().withArgs('@gamechanger/metrics.something', sinon.match.number, 1, ['foo:bar']);
-            metrics.timed(function(cb) {
-                setTimeout(function() {
-                    cb('something', {foo:'bar'});
-                    mockStatsD.verify();
-                    done();
-                }, 10);
-            });
-        });
+    it('should send histogram info with tags as array', () => {
+      mockStatsD.expects('histogram').once()
+          .withArgs('customPrefix.something', 50, 1, ['test']);
+      metrics.histogram('something', 50, ['test']);
+      mockStatsD.verify();
     });
 
+    it('should send histogram info with sample rate and tags as object', () => {
+      mockStatsD.expects('histogram').once()
+          .withArgs('customPrefix.something', 50, 0.5, ['foo:bar']);
+      metrics.histogram('something', 50, 0.5, {foo: 'bar'});
+      mockStatsD.verify();
+    });
+
+    it('should send histogram info with tags as object', () => {
+      mockStatsD.expects('histogram').once()
+          .withArgs('customPrefix.something', 50, 1, ['foo:bar']);
+      metrics.histogram('something', 50, {foo: 'bar'});
+      mockStatsD.verify();
+    });
+  });
+
+  describe('timed callback', () => {
+    it('should time callbacks with sample rate and tag array', (done) => {
+      mockStatsD.expects('timing').once()
+          .withArgs(
+              'customPrefix.something',
+              sinon.match.number,
+              0.5, ['test'],
+          );
+      metrics.timed((cb) => {
+        setTimeout(() => {
+          cb('something', 0.5, ['test']);
+          mockStatsD.verify();
+          done();
+        }, 10);
+      });
+    });
+
+    it('should time callbacks with tag array', (done) => {
+      mockStatsD.expects('timing').once()
+          .withArgs(
+              'customPrefix.something',
+              sinon.match.number,
+              1,
+              ['test'],
+          );
+      metrics.timed((cb) => {
+        setTimeout(() => {
+          cb('something', ['test']);
+          mockStatsD.verify();
+          done();
+        }, 10);
+      });
+    });
+
+    it('should time callbacks with sample rate and tag object', (done) => {
+      mockStatsD.expects('timing').once()
+          .withArgs(
+              'customPrefix.something',
+              sinon.match.number,
+              0.5,
+              ['foo:bar'],
+          );
+      metrics.timed((cb) => {
+        setTimeout(() => {
+          cb('something', 0.5, {foo: 'bar'});
+          mockStatsD.verify();
+          done();
+        }, 10);
+      });
+    });
+
+    it('should time callbacks with tag object', (done) => {
+      mockStatsD.expects('timing').once()
+          .withArgs(
+              'customPrefix.something',
+              sinon.match.number,
+              1,
+              ['foo:bar'],
+          );
+      metrics.timed((cb) => {
+        setTimeout(() => {
+          cb('something', {foo: 'bar'});
+          mockStatsD.verify();
+          done();
+        }, 10);
+      });
+    });
+  });
 });
